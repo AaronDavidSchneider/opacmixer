791a1d687ff679d1fd4927e88dc2375be7482323
Aaron David Schneider <aaron.schneider@nbi.ku.dk>
2023-02-02 16:30:52 +0100

    RORR seems working, first speedups done

--- opac_mixer/mix.py
+++ opac_mixer/mix.py
@@ -1,5 +1,20 @@
+import numba
 import numpy as np
-from scipy.signal import oaconvolve
+
+
+@numba.njit(nogil=True, fastmath=True, cache=True)
+def resort_rebin_njit(ggrid, kout_conv, weights_conv, kout, Np, Nt, Nf, Ng):
+    for pi in numba.prange(Np):
+        for ti in numba.prange(Nt):
+            for freqi in numba.prange(Nf):
+                index_sort = np.argsort(kout_conv[pi,ti,freqi])
+                kout_conv_resorted = kout_conv[pi,ti,freqi][index_sort]
+                weights_resorted = weights_conv[index_sort]
+                g_resorted = weights_resorted.cumsum()
+                kout[pi, ti, freqi, :] = np.interp(ggrid, g_resorted,
+                                                   kout_conv_resorted)
+    return kout
+
 
 class CombineOpac:
     def __init__(self, opac):
@@ -17,7 +32,7 @@ class CombineOpac:
         elif method == 'AEE':
             return self._add_aee(self.opac.kcoeff, mmr)
         elif method == 'RORR':
-            return self._add_rorr(self.opac.kcoeff, self.opac.weights.cumsum(), mmr)
+            return self._add_rorr(self.opac.kcoeff, self.opac.weights, mmr)
         else:
             raise NotImplementedError('Method not implemented.')
 
@@ -27,21 +42,28 @@ class CombineOpac:
 
     @staticmethod
     def _add_aee(ktable, mmr):
-        raise NotImplementedError('Method not implemented.')
+        weighted_k = ktable*mmr[:,:,:,np.newaxis,np.newaxis]
+        gray_k = np.sum(weighted_k,axis=-1)
+        index_max = np.argsort(gray_k, axis=0)
+        major = np.take_along_axis(weighted_k, index_max[..., np.newaxis], 0)[-1]
+        rest = np.sum(np.take_along_axis(gray_k, index_max, 0)[:-1], axis=0)
+        return major + rest[..., np.newaxis]
 
     @staticmethod
-    def _add_rorr(ktable, g, mmr):
+    def _add_rorr(ktable, weights, mmr):
         """Add up ktables by random overlap with resorting and rebinning."""
-        kout = mmr[0,:,:,np.newaxis,np.newaxis]*ktable[0]
-        g_conv = np.convolve(g,g)
-        sort_index = np.argsort(g_conv)
-        g_conv_sorted = g_conv[sort_index]
-
-        interp_j = np.searchsorted(g_conv_sorted, g) - 1
-        interp_d = (g - g_conv_sorted[interp_j]) / (g_conv_sorted[interp_j + 1] - g_conv_sorted[interp_j])
+        kout = mmr[0,:,:,np.newaxis,np.newaxis]*ktable[0,:,:,:,:]
+        ggrid = weights.cumsum()
+        weights_conv = np.outer(weights, weights).flatten()
 
         for speci in range(ktable.shape[0]-1):
-            kout_conv = oaconvolve(kout, ktable[speci+1]*mmr[speci+1,:,:,np.newaxis,np.newaxis], axes=-1)[..., sort_index]
-            kout = (1 - interp_d) * kout_conv[:,:,:,interp_j] + kout_conv[:,:,:,interp_j] * interp_d
-        
+            # t0 = time.time()
+            kout_conv = (kout[...,:,None]+
+                         (ktable[speci + 1] * mmr[speci + 1, :, :, np.newaxis, np.newaxis])[...,None,:])\
+                .reshape(*kout.shape[:-1], weights_conv.shape[0])
+
+            # t1 = time.time()
+            kout = resort_rebin_njit(ggrid, kout_conv, weights_conv, kout, *kout.shape)
+            # print('time (total, interp, relinterp):',time.time()-t0,time.time()-t1, (time.time()-t1)/(time.time()-t0))
+
         return kout
